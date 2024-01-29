f94a573e7c3254197e3370ce764842c6c2ac020c
tommya98 <tommya98@naver.com>
2024-01-29 16:02:05 +0900

    STYLE: 글로벌 스타일 배경색 변경

--- src/styles/GlobalStyles/index.tsx
+++ src/styles/GlobalStyles/index.tsx
@@ -7,7 +7,7 @@ const style = (theme: Theme) => css`
       BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
       "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
       "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
-    background-color: ${theme.SECONDARY};
+    background-color: ${theme.COMPLEMENTARY};
     text-decoration: none;
     color: ${theme.TEXT};
   }
