4fba14c7f7785f8200360a1b9ae818b9cf26eb6a
tommya98 <tommya98@naver.com>
2024-02-03 17:19:37 +0900

    FEAT: 랜덤 전시물 렌더링 적용

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,13 +2,26 @@ import * as S from "./styled";
 
 import ShowcaseList from "../ShowcaseList";
 import exhibits from "../../exhibits";
+import selectExhibits from "../../utils/selectExhibits";
+
+const ARRAY_NUM = 3;
+const ITEM_NUM = 5;
 
 const Main = () => {
+  const selectedExhibits = selectExhibits(exhibits, ARRAY_NUM, ITEM_NUM);
+
   return (
     <S.Wrapper>
-      <ShowcaseList childrens={exhibits} direction="up" />
-      <ShowcaseList childrens={exhibits} direction="down" />
-      <ShowcaseList childrens={exhibits} direction="up" />
+      {selectedExhibits.map((exhibits, i) => {
+        const direction = i % 2 === 0 ? "up" : "down";
+        return (
+          <ShowcaseList
+            key={`exhibit_${i}`}
+            childrens={exhibits}
+            direction={direction}
+          />
+        );
+      })}
     </S.Wrapper>
   );
 };
