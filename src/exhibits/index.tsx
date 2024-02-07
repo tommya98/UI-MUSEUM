cd20c79cca85e15fcfd7fe8b87e0c77d179efb63
tommya98 <tommya98@naver.com>
2024-02-07 19:41:59 +0900

    REFACTOR: 전시물 컴포넌트 데이터 분리

--- src/exhibits/index.tsx
+++ src/exhibits/index.tsx
@@ -1,81 +1,8 @@
 import React, { ReactNode } from "react";
 
-import MyComponent from "./MyComponent";
-import DontClickButton from "./DontClickButton";
-
-interface IComponentsWithMetaData {
-  component: React.ComponentType<any>;
-  props?: Record<string, any>;
-  metaData: {
-    title: string;
-    description: string;
-    date: string;
-    url: string;
-  };
-}
+import componentsWithMetaData from "./data";
 
 const exhibits: ReactNode[] = [];
-
-const componentsWithMetaData: IComponentsWithMetaData[] = [
-  {
-    component: MyComponent,
-    props: {
-      bg: "red",
-    },
-    metaData: {
-      title: "기본 빨강 버튼",
-      description: "테스트용 기본 빨강 버튼입니다.",
-      date: "2024-02-02",
-      url: "https://github.com/tommya98/UI-MUSEUM/tree/main/src/exhibits/MyComponent",
-    },
-  },
-  {
-    component: MyComponent,
-    props: {
-      bg: "blue",
-    },
-    metaData: {
-      title: "기본 파랑 버튼",
-      description: "테스트용 기본 파랑 버튼입니다.",
-      date: "2024-02-02",
-      url: "https://github.com/tommya98/UI-MUSEUM/tree/main/src/exhibits/MyComponent",
-    },
-  },
-  {
-    component: MyComponent,
-    props: {
-      bg: "green",
-    },
-    metaData: {
-      title: "기본 초록 버튼",
-      description: "테스트용 기본 초록 버튼입니다.",
-      date: "2024-02-02",
-      url: "https://github.com/tommya98/UI-MUSEUM/tree/main/src/exhibits/MyComponent",
-    },
-  },
-  {
-    component: MyComponent,
-    props: {
-      bg: "orange",
-    },
-    metaData: {
-      title: "기본 주황 버튼",
-      description: "테스트용 기본 주황 버튼입니다.",
-      date: "2024-02-02",
-      url: "https://github.com/tommya98/UI-MUSEUM/tree/main/src/exhibits/MyComponent",
-    },
-  },
-  {
-    component: DontClickButton,
-    props: {},
-    metaData: {
-      title: "누르면 안 되는 버튼",
-      description: "누르면 안되지만 혹시라도 누르면 깜빡이는 버튼입니다.",
-      date: "2024-02-05",
-      url: "https://github.com/tommya98/UI-MUSEUM/tree/main/src/exhibits/DontClickButton",
-    },
-  },
-];
 
 componentsWithMetaData.forEach(({ component, props, metaData }) => {
   exhibits.push(React.createElement(component, { ...props, ...metaData }));
